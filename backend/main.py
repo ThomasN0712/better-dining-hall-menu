84fe2fd270fd7a081eb1dd53c3278e1ff2992abd
Thomas Nguyen <92189845+ThomasN0712@users.noreply.github.com>
2024-11-26 02:26:37 -0800

    silly me

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,6 @@ app.add_middleware(
         "http://localhost:3000",  # Local development
         "https://better-dining-hall-menu.vercel.app/" # Production
     ],
-    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
